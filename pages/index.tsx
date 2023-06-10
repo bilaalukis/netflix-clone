2c3951e8ea43e8e16af8070adb5f46183f2f2bfe
Bilaal <73635058+bilaalukis@users.noreply.github.com>
2023-06-10 14:32:06 +0000

    server-side rendering of movie list

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,76 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Header from "@/components/Header";
+import Banner from "@/components/Banner";
+import requests from "@/utils/request";
+import { Movie } from "@/typings";
 
-const inter = Inter({ subsets: ["latin"] });
+interface Props {
+  netflixOriginals: Movie[];
+  trendingNow: Movie[];
+  topRated: Movie[];
+  actionMovies: Movie[];
+  comedyMovies: Movie[];
+  horrorMovies: Movie[];
+  romanceMovies: Movie[];
+  documentaries: Movie[];
+}
 
-export default function Home() {
+export default function Home({
+  netflixOriginals,
+  actionMovies,
+  comedyMovies,
+  documentaries,
+  horrorMovies,
+  romanceMovies,
+  topRated,
+  trendingNow,
+}: Props) {
+  console.log(netflixOriginals);
   return (
-    <>
+    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Header />
-      <main></main>
-    </>
+
+      <main>
+        <Banner />
+
+        <section></section>
+      </main>
+    </div>
   );
 }
+
+export const getServerSideProps = async () => {
+  const [
+    netflixOriginals,
+    trendingNow,
+    topRated,
+    actionMovies,
+    comedyMovies,
+    horrorMovies,
+    romanceMovies,
+    documentaries,
+  ] = await Promise.all([
+    fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
+    fetch(requests.fetchTrending).then((res) => res.json()),
+    fetch(requests.fetchTopRated).then((res) => res.json()),
+    fetch(requests.fetchActionMovies).then((res) => res.json()),
+    fetch(requests.fetchComedyMovies).then((res) => res.json()),
+    fetch(requests.fetchHorrorMovies).then((res) => res.json()),
+    fetch(requests.fetchRomanceMovies).then((res) => res.json()),
+    fetch(requests.fetchDocumentaries).then((res) => res.json()),
+  ]);
+
+  return {
+    props: {
+      netflixOriginals: netflixOriginals.results,
+      trendingNow: trendingNow.results,
+      topRated: topRated.results,
+      actionMovies: actionMovies.results,
+      comedyMovies: comedyMovies.results,
+      horrorMovies: horrorMovies.results,
+      romanceMovies: romanceMovies.results,
+      documentaries: documentaries.results,
+    },
+  };
+};
